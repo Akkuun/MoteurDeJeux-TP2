f20154cdc0ddf5f39c6f12db6cbd58e2ea9197c7
Akkuun <mathisduban@gmail.com>
2025-02-13 10:33:23 +0100

    first commit

---
+++ TP1/vertex_shader.glsl
@@ -0,0 +1,19 @@
+#version 330 core
+
+// Input vertex data, different for all executions of this shader.
+layout(location = 0) in vec3 vertices_position_modelspace;
+
+//TODO create uniform transformations matrices Model View Projection
+uniform mat4 MVP;
+
+// Values that stay constant for the whole mesh.
+
+
+void main(){
+
+        // TODO : Output position of the vertex, in clip space : MVP * position
+        gl_Position = MVP * vec4(vertices_position_modelspace,1);
+
+
+}
+
